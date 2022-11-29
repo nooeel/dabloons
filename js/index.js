722ede37ec2276324127f572c24d50fd125152ac
Noel Leon <noel.glaeser@gmail.com>
2022-11-30 00:07:31 +0100

    player creation + rendering

--- js/index.js
+++ js/index.js
@@ -8,11 +8,28 @@ c.fillStyle = 'white'
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
-const image = new Image()
-image.src = './assets/images/townOne.png'
+const bgImg = new Image()
+bgImg.src = './assets/images/townOne.png'
 
-image.onload = () => {
-    c.drawImage(image, -750, -550)
+const playerImg = new Image();
+playerImg.src = './assets/images/playerDown.png'
+
+playerImg.onload = () => {
+    c.drawImage(
+        bgImg, 
+        -740, 
+        -600
+    )
+
+    c.drawImage(
+        playerImg, 
+        0,
+        0,
+        playerImg.width / 4,
+        playerImg.height,
+        canvas.width / 2 - (playerImg.width / 4) / 2, 
+        canvas.height / 2 - playerImg.height / 4,
+        playerImg.width / 4,
+        playerImg.height
+    )
 }
-
-
