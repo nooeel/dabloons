d90d516eb923d085fe4889ce123a0c8ab5644e84
Noel Leon <noel.glaeser@gmail.com>
2022-11-29 23:37:09 +0100

    image sichtbar onload

--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ const c = canvas.getContext('2d')  //context
 
 canvas.width = 1024
 canvas.height = 576
-console.log(c)
+
+c.fillStyle = 'white'
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './assets/images/townOne.png'
+
+image.onload = () => {
+    c.drawImage(image, 0, 0)
+}
+
+
