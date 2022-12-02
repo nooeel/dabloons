823abc9a04c14f3d3e4a47fc7d4aaf51ae8c74a8
Noel Leon <noel.glaeser@gmail.com>
2022-12-02 10:18:20 +0100

    testboundary finish

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ canvas.width = 1024
 canvas.height = 576
 
 const offset = {
-    x: -740,
+    x: -713,
     y: -600
 }
 
@@ -57,26 +57,49 @@ collisionsMap.forEach((row, i) => {
     })
 })
 
+const testBoundary = new Boundary({
+    position: {
+        x: 400,
+        y: 400
+    }
+})
 
 
 
+const townOneImgRaw = new Image()
+townOneImgRaw.src = 'assets/Images/townOne.png'
 
-const townImage = new Image()
-townImage.src = 'assets/Images/townOne.png'
-
-const playerImage = new Image();
-playerImage.src = 'assets/Images/playerDown.png'
+const playerImgRaw = new Image();
+playerImgRaw.src = 'assets/Images/playerDown.png'
 
 
 
 class Sprite {
-    constructor({position, velocity, image}) {
+    constructor({position, velocity, image, frames = {max: 1} }) {
         this.position = position
         this.image = image
+        this.frames = frames
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height
+        }
+
+        
     }
 
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.drawImage(
+            this.image, 
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height
+        )
     }
 }
 
@@ -86,12 +109,26 @@ class Sprite {
 
 
 
-const townImg = new Sprite({
+
+
+const townOneBg = new Sprite({
     position: {
         x: offset.x,
         y: offset.y
     },
-    image: townImage
+    image: townOneImgRaw
+})
+
+
+const player = new Sprite({
+    position: {
+        x: canvas.width / 2 - (playerImgRaw.width / 4) / 2,
+        y:canvas.height / 2 - playerImgRaw.height / 4
+    },
+    image: playerImgRaw,
+    frames: {
+        max: 4
+    }
 })
 
 
@@ -112,6 +149,18 @@ const keys = {
 }
 
 
+const movables = [townOneBg, testBoundary]
+
+function rectengularCollision({rectangle1, rectangle2}) {
+    return( 
+        player.position.x + player.width >= testBoundary.position.x &&
+        player.position.x <= testBoundary.position.x + testBoundary.width &&
+        player.position.y + player.height >= testBoundary.position.y &&    
+        player.position.y <= testBoundary.position.y + testBoundary.height
+    )
+}
+
+
 
 
 function loop() {
@@ -123,54 +172,57 @@ loop();
 
 
 
-const testBoundary = new Boundary({
-    position: {
-        x: 400,
-        y: 400
-    }
-})
+
 
 
 function render() {
     
-    townImg.draw()
+    townOneBg.draw()
 
     if (development === true) {
         showBoundary()
     }
+
+    testBoundary.draw()
+
+    player.draw()
     
-    c.drawImage(
-        playerImage, 
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - (playerImage.width / 4) / 2, 
-        canvas.height / 2 - playerImage.height / 4,
-        playerImage.width / 4,
-        playerImage.height
-    )
+    
 }
 
 
 function showBoundary() {
-
-    
-    
     boundaries.forEach(boundary => {
         boundary.draw()
-    })
-    
-    
+    })   
 }
 
 
 
+
+
+function moving() {
+         if (keys.w.pressed && lastKey === 'w') movables.forEach(movable => {movable.position.y += 5}) 
+    else if (keys.a.pressed && lastKey === 'a') movables.forEach(movable => {movable.position.x += 5})
+    else if (keys.s.pressed && lastKey === 's') movables.forEach(movable => {movable.position.y -= 5})
+    else if (keys.d.pressed && lastKey === 'd') movables.forEach(movable => {movable.position.x -= 5})
+}
+
+
+
+
 function gameLogic() {
-         if (keys.w.pressed && lastKey === 'w') townImg.position.y += 5
-    else if (keys.a.pressed && lastKey === 'a') townImg.position.x += 5
-    else if (keys.s.pressed && lastKey === 's') townImg.position.y -= 5
-    else if (keys.d.pressed && lastKey === 'd') townImg.position.x -= 5
+    
+    if (
+        rectengularCollision({
+            rectangle1: player,
+            rectangle2: testBoundary
+        })
+    ) {
+        console.log('collide');
+    }
+
+    moving()
 }
 
 
