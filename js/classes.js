54046741e3b9210d4944965209c5eb780095a162
Noel Leon <noel.glaeser@gmail.com>
2022-12-02 23:07:33 +0100

    fgobjects and organization classes

---
+++ js/classes.js
@@ -0,0 +1,48 @@
+let a = 1
+
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({position}) {
+        this.position = position
+        this.height = 48
+        this.width = 48
+    }
+
+    draw() {
+        //c.fillStyle = 'rgba(255, 0, 0, 0.2)'  //sichtbare boundary
+        c.fillStyle = 'rgba(255, 0, 0, 0)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.width)
+    }
+}
+
+
+
+class Sprite {
+    constructor({position, velocity, image, frames = {max: 1} }) {
+        this.position = position
+        this.image = image
+        this.frames = frames
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max
+            this.height = this.image.height
+        }
+
+        
+    }
+
+    draw() {
+        c.drawImage(
+            this.image, 
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height
+        )
+    }
+}
--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ const c = canvas.getContext('2d')  //context
 
 const development = true
 
-
-
 canvas.width = 1024
 canvas.height = 576
+
+
 
 const offset = {
     x: -713,
@@ -22,22 +22,6 @@ for (let i = 0; i < collisions.length; i+= 70) {
 }
 
 
-
-
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({position}) {
-        this.position = position
-        this.height = 48
-        this.width = 48
-    }
-
-    draw() {
-        c.fillStyle = 'rgba(255, 0, 0, 0.2)'
-        c.fillRect(this.position.x, this.position.y, this.width, this.width)
-    }
-}
 
 const boundaries = []
 
@@ -59,49 +43,14 @@ collisionsMap.forEach((row, i) => {
 
 
 
-
-
 const townOneImgRaw = new Image()
 townOneImgRaw.src = 'assets/Images/townOne.png'
 
-const playerImgRaw = new Image();
+const fgTownOneImgRaw = new Image()
+fgTownOneImgRaw.src = 'assets/Images/fgTownOne.png'
+
+const playerImgRaw = new Image()
 playerImgRaw.src = 'assets/Images/playerDown.png'
-
-
-
-class Sprite {
-    constructor({position, velocity, image, frames = {max: 1} }) {
-        this.position = position
-        this.image = image
-        this.frames = frames
-
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max
-            this.height = this.image.height
-        }
-
-        
-    }
-
-    draw() {
-        c.drawImage(
-            this.image, 
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        )
-    }
-}
-
-
-
-
-
 
 
 
@@ -126,6 +75,14 @@ const player = new Sprite({
     }
 })
 
+const fgTownOne = new Sprite({
+    position: {
+        x: offset.x,
+        y: offset.y
+    },
+    image: fgTownOneImgRaw
+})
+
 
 
 const keys = {
@@ -144,7 +101,7 @@ const keys = {
 }
 
 
-const movables = [townOneBg, ...boundaries]
+const movables = [townOneBg, fgTownOne, ...boundaries]
 
 function rectengularCollision({rectangle1, rectangle2}) {
     return( 
@@ -171,17 +128,12 @@ loop();
 
 
 function render() {
-    
     townOneBg.draw()
 
-    if (development === true) {
-        showBoundary()
-    }
-
+    showBoundary()
 
     player.draw()
-    
-    
+    fgTownOne.draw()  
 }
 
 
@@ -290,8 +242,6 @@ function moving() {
 
 function gameLogic() {
     
-    
-
     moving()
 }
 
